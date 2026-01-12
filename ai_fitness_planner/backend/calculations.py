36c9717c76142459f0dcb3a41df20d9c835b92dc
SubhamChaudhuri001 <subhamchaudhuri.97@gmail.com>
2026-01-12 21:24:04 +0530

    Update calculations.py

--- ai_fitness_planner/backend/calculations.py
+++ ai_fitness_planner/backend/calculations.py
@@ -1,10 +1,12 @@
 # backend/calculations.py
-
 def calculate_bmi(weight, height):
     """
+    Safe BMI calculation
     weight: kg
     height: cm
     """
+    if height is None or height <= 0:
+        return None
     return weight / ((height / 100) ** 2)
 
 
@@ -16,3 +18,4 @@ def calculate_bmr(gender, weight, height, age):
         return 10 * weight + 6.25 * height - 5 * age + 5
     else:
         return 10 * weight + 6.25 * height - 5 * age - 161
+
