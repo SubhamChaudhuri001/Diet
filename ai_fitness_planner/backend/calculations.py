94f38d064ad589405a5137db9be8793c6016a1af
SubhamChaudhuri001 <subhamchaudhuri.97@gmail.com>
2026-01-12 19:27:47 +0530

    Add files via upload

---
+++ ai_fitness_planner/backend/calculations.py
@@ -0,0 +1,4 @@
+def calculate_bmr(gender, weight, height, age):
+    if gender == "Male":
+        return 10 * weight + 6.25 * height - 5 * age + 5
+    return 10 * weight + 6.25 * height - 5 * age - 161
